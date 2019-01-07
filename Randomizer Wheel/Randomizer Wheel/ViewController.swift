488abf146a2be4f891982fd400e5b80672119470
GaumerAlexander <34212464+GaumerAlexander@users.noreply.github.com>
2019-01-07 16:17:31 +0100

    Add files via upload

---
+++ Randomizer Wheel/Randomizer Wheel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Randomizer Wheel
+//
+//  Created by Alexander Gaumer on 07.01.19.
+//  Copyright © 2019 Alexander Gaumer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
